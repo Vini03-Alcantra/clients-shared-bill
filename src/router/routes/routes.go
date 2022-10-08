c36b897a3fa61edf1b83506c3e1a05ab790584fa
Vinícius de Alcantra <52285659+Vini03-Alcantra@users.noreply.github.com>
2022-10-07 23:49:32 -0300

    genrating route to controller

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,12 +1,13 @@
 package routes
 
 import (
+	client "github.com/ClientsSharedBill/src/controllers"
 	"github.com/gofiber/fiber"
 )
 
 func setupRoutes(app *fiber.App) {
-	app.Get("api/v1/client")
-	app.Get("api/v1/client/:id")
-	app.Post("api/v1/client")
-	app.Delete("api/v1/client/:id")
+	app.Get("api/v1/clients", client.GetClients)
+	app.Get("api/v1/clients/:id", client.GetClient)
+	app.Post("api/v1/clients", client.NewClient)
+	app.Delete("api/v1/clients/:id", client.DeleteClient)
 }

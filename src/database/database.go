7d306227c1e073d52ade716f690c2b0ea5949488
Vinícius de Alcantra <52285659+Vini03-Alcantra@users.noreply.github.com>
2022-10-08 15:42:44 -0300

    Removing unnecessary code

--- src/database/database.go
+++ src/database/database.go
@@ -6,21 +6,22 @@ import (
 	"os"
 	"time"
 
-	"github.com/ClientsSharedBill/src/models"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"github.com/ClientsSharedBill/src/models"
+	"github.com/joho/godotenv"
 )
 
 var dbConn *gorm.DB
-var StringConexaoBanco = ""
+var StringConnectionDatabase = ""
 
 func Connect() error {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	StringConexaoBanco = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+	StringConnectionDatabase = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
@@ -30,7 +31,7 @@ func Connect() error {
 	)
 
 	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  StringConexaoBanco,
+		DSN:                  StringConnectionDatabase,
 		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
 
@@ -46,33 +47,6 @@ func Connect() error {
 
 	sqlDB.SetConnMaxLifetime(time.Hour)
 	dbConn = db
-
-	return err
-}
-
-func GetDatabaseConnection() (*gorm.DB, error) {
-	sqlDB, err := dbConn.DB()
-
-	if err != nil {
-		return dbConn, err
-	}
-
-	if err := sqlDB.Ping(); err != nil {
-		return dbConn, err
-	}
-	AutoMigrate()
-	return dbConn, nil
-}
-
-func AutoMigrate() error {
-	// Auto Migrate database
-	db, connErr := GetDatabaseConnection()
-	if connErr != nil {
-		return connErr
-	}
-
-	// Add required models here
-	err := db.AutoMigrate(&models.Client{})
-	fmt.Print("Database Migrated")
+	dbConn.AutoMigrate(&models.Client{})
 	return err
 }

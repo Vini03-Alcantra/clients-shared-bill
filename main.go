d52e340d524554d378f220d85e732babdad787ce
Vinícius de Alcantra <52285659+Vini03-Alcantra@users.noreply.github.com>
2022-10-08 14:24:50 -0300

    generate route controller functions

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 	"strconv"
 
 	"github.com/ClientsSharedBill/src/database"
+	"github.com/ClientsSharedBill/src/router/routes"
+	"github.com/gofiber/fiber"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
+	app := fiber.New()
 	database.Connect()
-
+	routes.SetupRoutes(app)
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
@@ -21,6 +24,6 @@ func main() {
 	if erro != nil {
 		PORT = 3032
 	}
-	log.Println(PORT)
-	// log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT)))
+
+	app.Listen(PORT)
 }
--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gofiber/fiber"
 )
 
-func setupRoutes(app *fiber.App) {
+func SetupRoutes(app *fiber.App) {
 	app.Get("api/v1/clients", client.GetClients)
 	app.Get("api/v1/clients/:id", client.GetClient)
 	app.Post("api/v1/clients", client.NewClient)
